7ddcbd47bdfe8298a73a6514399cd8bbb8ab1fff
Yijing Chen <yijing@duolingo.com>
2020-08-22 10:45:29 -0400

    Create server command.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { CommandGroup, CommandoClient } from "discord.js-commando";
 
 import { CAP_GROUP_ID } from "./cap/capGroup";
+import TestServerCommand from "./cap/testServer";
 import { PREFIX } from "./prefix";
 
 const init = async () => {
@@ -10,6 +11,7 @@ const init = async () => {
   });
   client.registry
     .registerDefaults()
+    .registerCommand(new TestServerCommand(client))
     .registerGroup(new CommandGroup(client, CAP_GROUP_ID, "CAP"));
 
   await client.login(process.env.CAP_DISCORD_BOT_TOKEN);
