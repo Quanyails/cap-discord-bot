6c0bdddabb7d5890c034b0f230b164da5ffac64c
Yijing Chen <yijing@duolingo.com>
2020-08-23 10:39:33 -0400

    Create PokemonArray type.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { CommandGroup, CommandoClient } from "discord.js-commando";
 
 import FormatType, { FORMAT_TYPE_ID } from "./arguments/format";
 import PokemonType, { POKEMON_TYPE_ID } from "./arguments/pokemon";
+import PokemonArrayType, {
+  POKEMON_ARRAY_TYPE_ID,
+} from "./arguments/pokemonArray";
 import TierType, { TIER_TYPE_ID } from "./arguments/tier";
 import TypeType, { TYPE_TYPE_ID } from "./arguments/type";
 import BsrCommand from "./bsr/bsr";
@@ -36,6 +39,7 @@ const init = async () => {
     .registerGroup(new CommandGroup(client, CAP_GROUP_ID, "CAP"))
     .registerGroup(new CommandGroup(client, PS_GROUP_ID, "PS"))
     .registerType(new FormatType(client, FORMAT_TYPE_ID))
+    .registerType(new PokemonArrayType(client, POKEMON_ARRAY_TYPE_ID))
     .registerType(new PokemonType(client, POKEMON_TYPE_ID))
     .registerType(new TierType(client, TIER_TYPE_ID))
     .registerType(new TypeType(client, TYPE_TYPE_ID))
